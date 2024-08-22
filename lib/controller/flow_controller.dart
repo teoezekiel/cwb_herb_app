8b7927e89ccb9bbf763577df123f1375b9343ec0
Isaac-Talb <thaungahlaybaw@gmail.com>
2024-08-22 07:45:59 +0630

    del

--- lib/controller/flow_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class FlowController extends GetxController {
-  int currentFlow = 1;
-  void setFlow(int flow) {
-    currentFlow = flow;
-    update();
-  }
-}
--- lib/models/file_model.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:typed_data';
-
-class FileModel {
-  String filename;
-  Uint8List fileBytes;
-
-  FileModel({required this.filename, required this.fileBytes});
-}
